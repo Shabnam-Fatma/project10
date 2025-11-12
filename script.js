0c7fc825ff18c8de03165d6797192ab0c274d269
Shabnam Fatma <shabnamfatma.tech@gmail.com>
2025-11-12 22:42:13 +0530

    initial commit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const cartIcon = document.querySelector(".cart-icon");
 const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
 const cardList = document.querySelector(".card-list");
+const cartList = document.querySelector(".cart-list");
 
 cartIcon.addEventListener("click", () =>
   cartTab.classList.add("cart-tab-active")
@@ -25,15 +26,46 @@ const showCards = () => {
     const orderCard = document.createElement("div");
     orderCard.classList.add("order-card");
     orderCard.innerHTML = `
-    <div class="card-image">
+      <div class="card-image">
+        <img src="${product.image}">
+      </div>
+      <h4>${product.name}</h4>
+      <h4 class="price">${product.price}</h4>
+      <a href="#" class="btn card-btn">Add to Cart</a>
+    `;
+    cardList.appendChild(orderCard);
+
+    const cardBtn = orderCard.querySelector(".card-btn");
+    cardBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      addToCart(product);
+    });
+  });
+};
+
+const addToCart = (product) => {
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("item");
+  cartItem.innerHTML = `
+    <div class="item-image">
       <img src="${product.image}">
     </div>
-    <h4>${product.name}</h4>
-    <h4 class="price">${product.price}}</h4>
-    <a href="#" class="btn">Add to Cart</a>
-    `;
-    cardList.appendChild(orderCard)
-  });
+    <div class="detail">
+      <h4>${product.name}</h4>
+      <h4 class="price">${product.price}</h4>
+    </div>
+    <div class="flex">
+      <a href="#" class="quantity-btn">
+        <i class="fa-solid fa-minus"></i>
+      </a>
+      <h4 class="quantity-value">1</h4>
+      <a href="#" class="quantity-btn">
+        <i class="fa-solid fa-plus"></i>
+      </a>
+    </div>
+  `;
+
+  cartList.appendChild(cartItem);
 };
 
 const initApp = () => {
